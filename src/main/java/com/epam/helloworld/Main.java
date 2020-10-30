45fd21450a1119ca26cc113dc539dcfb19c43aba
Andrei Kaliachka <andreikaliachka@Andreis-MBP.minsk.epam.com>
2020-10-30 19:33:22 +0300

    dsadsc

--- src/main/java/com/epam/helloworld/Main.java
+++ src/main/java/com/epam/helloworld/Main.java
@@ -3,5 +3,6 @@ package com.epam.helloworld;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Change something");
     }
 }
